301bda81089d71c01e4b911b5ef9bf660d62a322
Collins Njogu <njogucollins10397@gmail.com>
2025-12-03 15:54:24 +0300

    wozaa

--- src/components/blog.jsx
+++ src/components/blog.jsx
@@ -2,7 +2,6 @@
 import React, { useState } from "react";
 import { motion, AnimatePresence } from "framer-motion";
 
-// Sample case studies
 const caseStudies = [
   {
     title: "Reverse Linked List",
